1722da198fdec04ebe9d90b4c113f6f3c9ab9a96
Rocco Panacci <n01340339@nj-00118-001.humber.org>
2020-03-10 16:42:35 -0400

    Some quick DOM manipulation

---
+++ 10/dom-practice/js/index.js
@@ -0,0 +1,11 @@
+let helloHeading = document.querySelector('#hello')
+
+// Update content
+helloHeading.textContent = 'Goodbye for now'
+
+// Update single CSS property
+helloHeading.style.color = 'tomato'
+helloHeading.style.backgroundColor = 'lightgreen'
+
+// Update class=""
+helloHeading.className = 'whatever goodbye'
